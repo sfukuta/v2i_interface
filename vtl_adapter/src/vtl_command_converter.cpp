f5e839a38a318d9ba78bc45f272a2d0617edbe07
Shigekazu Fukuta <107168699+sfukuta@users.noreply.github.com>
2023-05-18 16:37:41 +0900

    Update vtl_adapter/src/vtl_command_converter.cpp
    Co-authored-by: Yuma Nihei <yuma.nihei@tier4.jp>

--- vtl_adapter/src/vtl_command_converter.cpp
+++ vtl_adapter/src/vtl_command_converter.cpp
@@ -88,7 +88,7 @@ std::shared_ptr<InterfaceConverterMultiMap> VtlCommandConverter::createConverter
       RCLCPP_DEBUG(node_->get_logger(),
         "VtlCommandConverter:%s: ignore command::NONE.", __func__);
       continue;
-    }else if (orig_elem.state == MainInputCommand::FINALIZED) {
+    } else if (orig_elem.state == MainInputCommand::FINALIZED) {
       continue;
     }
     const auto converter(new InterfaceConverter(orig_elem, node_));
