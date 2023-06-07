2f0b3d9e4cccd4780b297e1e59554dc3f61a52a1
Shigekazu Fukuta <107168699+sfukuta@users.noreply.github.com>
2023-06-07 16:09:02 +0900

    Update vtl_adapter/src/vtl_state_converter.cpp

--- vtl_adapter/src/vtl_state_converter.cpp
+++ vtl_adapter/src/vtl_state_converter.cpp
@@ -87,7 +87,12 @@ void VtlStateConverter::onCommand(const MainInputCommandArr::ConstSharedPtr msg)
   const auto& cmd_arr = msg->commands;
   const auto is_all_commands_finalized =
     (std::count_if(cmd_arr.begin(), cmd_arr.end(), isNotFinalized) == 0);
-  const auto output_state = createState(is_all_commands_finalized);
+  OutputStateArr output_state;
+  if (is_all_commands_finalized) {
+     output_state.stamp = state_->stamp;
+  }else{
+     output_state = createState();
+  }
   if (!output_state) {
     RCLCPP_DEBUG(node_->get_logger(),
       "VtlStateConverter:%s: no valid state is found.", __func__);
